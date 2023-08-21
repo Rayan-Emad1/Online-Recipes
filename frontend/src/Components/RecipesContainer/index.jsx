3ccb1a1ea84e1ae3e01f0b18fe37397390c92caa
Rayan <rayanemad508@gmail.com>
2023-08-21 14:28:20 +0300

    FE - added title for evey container

--- frontend/src/Components/RecipesContainer/index.jsx
+++ frontend/src/Components/RecipesContainer/index.jsx
@@ -3,50 +3,61 @@ import Recipe from '../RecipeCard';
 import './styles.css';
 import RecipeInfo from '../RecipeInfo';
 
-
-const RecipesContainer = ({ url, islist = true }) => {
+const RecipesContainer = ({ url, islist = true, searchRecipe }) => {
   const [recipes, setRecipes] = useState([]);
   const [recipe_id, setRecipeId] = useState(null);
   const token = localStorage.getItem('token');
+  const [headerContent, setHeaderContent] = useState(null);
 
-  const getRecipes = () => {
-    fetch(`http://127.0.0.1:8000/api${url}`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (url === '/personal_info') {
-        if (islist) {
-          setRecipes(data.list_recipes);
+  const fetchData = () => { 
+    if (searchRecipe && searchRecipe.length > 0) {
+      setHeaderContent(<h1>Search Results</h1>);
+      setRecipes(searchRecipe);
+    } else {
+      fetch(`http://127.0.0.1:8000/api${url}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => response.json())
+      .then(data => {
+        let content;
+        if (url === '/personal_info') {
+          if (islist) {
+            content = <h1></h1>;
+            setRecipes(data.list_recipes);
+          } else {
+            content = <h1></h1>;
+            setRecipes(data.personal_recipes);
+          }
         } else {
-          setRecipes(data.personal_recipes);
+          content = <h1>Followers Recipes</h1>;
+          setRecipes(data.recipes);
         }
-      } else {
-        setRecipes(data.recipes);
-      }
-    })
-    .catch(error => {
-      console.log('Error:', error);
-    });
+        setHeaderContent(content);
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
+    }
   };
 
   useEffect(() => {
-    getRecipes();
-  }, [url, islist]);
+    fetchData();
+  }, [url, islist, searchRecipe, token]);
 
   return (
-    <>
+    <>  
       <div className="content">
+        {headerContent}
         <div className="recipe-container">
           {recipes.length === 0 ? (
-            islist ? <div>No List Recipes...</div> : <div>No Personal Recipes...</div>
+            <div> No Recipes...</div> 
           ) : (
             recipes.map(recipe => (
-              <Recipe key={recipe.id} recipe={recipe} getRecipes={getRecipes} setRecipeId={setRecipeId} islist={islist}  />
+              <Recipe key={recipe.id} recipe={recipe} getRecipes={() => fetchData()} setRecipeId={setRecipeId} islist={islist}  />
             ))
           )}
         </div>
